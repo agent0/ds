b1816f4079ac52d1a3bb56d7a356fe3c797f2ead
jli <Juergen.Lind@iteratec.de>
2017-06-17 10:54:59 +0200

    more tests

---
+++ src/test/java/de/agentlab/ds/table/TestMerge.java
@@ -0,0 +1,30 @@
+package de.agentlab.ds.table;
+
+import de.agentlab.ds.Table;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class TestMerge extends BaseTableTest {
+
+    public void testMerge() {
+        Table<String, String, String> t = new Table<>();
+
+        t.put("r1", "c1", "v");
+        t.put("r1", "c2", "v");
+        t.put("r1", "c3", "v");
+        t.put("r2", "c2", "v");
+
+        Table<String, String, String> t2 = new Table<>();
+
+        t2.put("r2", "c1", "v");
+        t2.put("r2", "c2", "v");
+        t2.put("r2", "c3", "v");
+        t2.put("r3", "c2", "v");
+
+        t.merge(t2);
+
+        Assert.assertEquals(t.toCsv(), ";c1;c2;c3;\nr1;v;v;v;\nr2;v;v;v;\nr3;;v;;\n");
+    }
+}
